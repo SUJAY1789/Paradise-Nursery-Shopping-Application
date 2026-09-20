1a3ea9008b3b83c5f1da98e25f3ac8e362052501
SUJAY1789 <sujaykarad2005@gmail.com>
2026-09-20 11:50:09 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
