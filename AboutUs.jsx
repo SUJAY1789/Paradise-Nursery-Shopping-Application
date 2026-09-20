df06119bfafbed9e24a887a72494cdddea9913ea
SUJAY1789 <sujaykarad2005@gmail.com>
2026-09-20 11:48:31 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shop that offers a variety of
+        beautiful houseplants for your home and indoor spaces.
+      </p>
+      <p>
+        Our goal is to make it easy for plant lovers to explore and purchase
+        plants while bringing more greenery into their everyday lives.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
